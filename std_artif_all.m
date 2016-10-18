65f2a17d8d3dd4774cc3e18c29f41b1350755557
P Zhao <peng.zhao@plymouth.ac.uk>
2016-10-18 23:06:18 +0100

    Add files via upload

--- std_artif_all.m
+++ std_artif_all.m
@@ -4,7 +4,7 @@ function std_artif_all
 
 disp('After modify arti for each subject, now running artifact rejection for all subjects ...')
 
-global nraw subject eegrefch avg erprefch meas gavg
+global nraw subject
 
 for s=1:nraw
         fprintf('\n++++\n+ Begin artifact rejection for %s ... \n++++\n\n', subject{s}); 
@@ -13,29 +13,3 @@ for s=1:nraw
         fd = str2func(f);
         fd(s);
 end
-
-% Optional: For EEGLab processing only; NOT part of standard ERP processing. NOTE: NEVER RE-REFERENCE TWICE.
-if eegrefch
-   std_eegrefch 
-end
-
-%Average ERP
-if avg
-    std_avg 
-end
-
-%ERPrefch: NOTE: NEVER RE-REFERENCE TWICE.
-if erprefch
-    std_erprefch
-end
-
-%Measure ERPs
-if meas
-    std_meas
-end
-
-%GRAND AVERAGE. Include standard deviation.
-if gavg
-    std_gavg 
-end
-    
