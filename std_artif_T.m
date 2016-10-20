76dd7bdbc19111c6e54950cc25202265ef326130
hschendan <haline_schendan@fastmail.us>
2016-10-20 13:30:47 +0100

    Update std_artif_T.m
    % GARV: when running artif_one, add single line artifact detection functions after default artparm lines above. 
    % Also add these 3 lines to artif for each subject
    % before: EEG.setname= [subject{s} sfx x]; to plot EEG, pause to scroll EEG, before save
    % Uncomment the 2 lines to use; comment out to not run
            pop_eegplot( EEG, 1, 1, 1); 
            fprintf('\n+++++++++++++++\n+ Hit return to save or Ctrl-c to quit and try again with new threshold ... \n+++++++++++++++\n\n', sfx);
            pause();    % Pause to scroll EEG plot
    % delete above 3 lines when all parms done, to run artif_all

--- std_artif_T.m
+++ std_artif_T.m
@@ -16,21 +16,13 @@ fprintf('\n+++++++++++++++\n+ (artif_T) Previous sfx = %s ... \n+++++++++++++++\
     % Artifact detection notes: 
     % BLINK & HEOG: Step-like. Test window = [-100 898] ms; Threshold = TODO uV; Window width = 400 ms (use default or TODO); Window step = 10 ms; Channels = 65 (Blink); Flags to be activated = 1 & 3
          if ch64 % 64 channel cap            
-            EEG = pop_artextval( EEG, 'Channel', [1:64,70:71], 'Flag',  2, 'Review', 'off', 'Threshold', [ artparm(s,1) artparm(s,2)], 'Twindow', [-200 900]); %Voltage threshold
-            EEG = pop_artstep( EEG , 'Channel',  71, 'Flag', 3, 'Review', 'off', 'Threshold',  artparm(s,3), 'Twindow', [ -200 900], 'Windowsize',  artparm(s,4), 'Windowstep',  artparm(s,5) ); % step-like artifacts VEOG
-            EEG = pop_artstep( EEG , 'Channel',  70, 'Flag',  4, 'Review', 'off', 'Threshold',  artparm(s,6), 'Twindow', [ -200 900], 'Windowsize',  artparm(s,7), 'Windowstep',  artparm(s,8) ); % step-like artifacts HEOG
-            EEG = pop_artstep( EEG , 'Channel',  1:64, 'Flag',  5, 'Review', 'off', 'Threshold',  artparm(s,9), 'Twindow', [ -200 900], 'Windowsize',  artparm(s,10), 'Windowstep',  artparm(s,11) ); % step-like artifacts other chans
-            EEG = pop_artmwppth( EEG , 'Channel',  1:64, 'Flag',  6, 'Review', 'off', 'Threshold',  artparm(s,12), 'Twindow', [ -200 900], 'Windowsize',  artparm(s,13), 'Windowstep',  artparm(s,14) ); % P2P ALL
-            EEG = pop_artmwppth( EEG , 'Channel',  71, 'Flag',  7, 'Review', 'off', 'Threshold',  artparm(s,15), 'Twindow', [ -200 900], 'Windowsize',  artparm(s,16), 'Windowstep',  artparm(s,17) ); % P2P VOG           
-            EEG = pop_artdiff( EEG , 'Channel',  1:64, 'Flag',  8, 'Review', 'off', 'Threshold',  artparm(s,18), 'Twindow', [ -200 900] ); %Extreme spikiness
-            EEG = pop_artextval( EEG, 'Channel', [32], 'Flag',  2, 'Review', 'off', 'Threshold', [100 200], 'Twindow', [-200 900]); %Voltage threshold
-            EEG = pop_artextval( EEG, 'Channel', [34], 'Flag',  2, 'Review', 'off', 'Threshold', [100 150], 'Twindow', [-200 900]); %Voltage threshold
-            EEG = pop_artextval( EEG, 'Channel', [55], 'Flag',  2, 'Review', 'off', 'Threshold', [150 200], 'Twindow', [-200 900]); %Voltage threshold
-            EEG = pop_artextval( EEG, 'Channel', [58], 'Flag',  2, 'Review', 'off', 'Threshold', [100 200], 'Twindow', [-200 900]); %Voltage threshold
-            EEG = pop_artextval( EEG, 'Channel', [14], 'Flag',  2, 'Review', 'off', 'Threshold', [100 200], 'Twindow', [-200 900]); %Voltage threshold
-            EEG = pop_artextval( EEG, 'Channel', [17], 'Flag',  2, 'Review', 'off', 'Threshold', [100 200], 'Twindow', [-200 900]); %Voltage threshold
-            EEG = pop_artextval( EEG, 'Channel', [63], 'Flag',  2, 'Review', 'off', 'Threshold', [100 200], 'Twindow', [-200 900]); %Voltage threshold
-            EEG = pop_artextval( EEG, 'Channel', [4], 'Flag',  2, 'Review', 'off', 'Threshold', [100 200], 'Twindow', [-200 900]); %Voltage threshold
+EEG = pop_artextval( EEG, 'Channel', [1:71], 'Flag',  2, 'Review', 'off', 'Threshold', [ artparm(s,1) artparm(s,2)], 'Twindow', [-200 900]); %Voltage threshold
+EEG = pop_artstep( EEG , 'Channel',  71, 'Flag', 3, 'Review', 'off', 'Threshold',  artparm(s,3), 'Twindow', [ -200 900], 'Windowsize',  artparm(s,4), 'Windowstep',  artparm(s,5) ); % step-like artifacts VEOG
+EEG = pop_artstep( EEG , 'Channel',  70, 'Flag',  4, 'Review', 'off', 'Threshold',  artparm(s,6), 'Twindow', [ -200 900], 'Windowsize',  artparm(s,7), 'Windowstep',  artparm(s,8) ); % step-like artifacts HEOG
+EEG = pop_artstep( EEG , 'Channel',  [1:64,68:69], 'Flag',  5, 'Review', 'off', 'Threshold',  artparm(s,9), 'Twindow', [ -200 900], 'Windowsize',  artparm(s,10), 'Windowstep',  artparm(s,11) ); % step-like artifacts other chans
+EEG = pop_artmwppth( EEG , 'Channel',  [1:64,68:69], 'Flag',  6, 'Review', 'off', 'Threshold',  artparm(s,12), 'Twindow', [ -200 900], 'Windowsize',  artparm(s,13), 'Windowstep',  artparm(s,14) ); % P2P ALL
+EEG = pop_artmwppth( EEG , 'Channel',  71, 'Flag',  7, 'Review', 'off', 'Threshold',  artparm(s,15), 'Twindow', [ -200 900], 'Windowsize',  artparm(s,16), 'Windowstep',  artparm(s,17) ); % P2P VOG           
+EEG = pop_artdiff( EEG , 'Channel',  [1:64,68:69], 'Flag',  8, 'Review', 'off', 'Threshold',  artparm(s,18), 'Twindow', [ -200 900] ); %Extreme spikiness, eg. muscle
          else    % 32 channel cap: TO check
             EEG = pop_artextval( EEG, 'Channel', [1:32,38:39], 'Flag',  2, 'Review', 'off', 'Threshold', [ artparms(s,1) artparms(s,2)], 'Twindow', [-200 900]); %Voltage threshold
             EEG = pop_artstep( EEG , 'Channel',  39, 'Flag',  3, 'Review', 'off', 'Threshold',  artparms(s,3), 'Twindow', [ -200 900], 'Windowsize',  artparms(s,4), 'Windowstep',  artparms(s,5) ); % step-like artifacts VEOG
@@ -41,6 +33,15 @@ fprintf('\n+++++++++++++++\n+ (artif_T) Previous sfx = %s ... \n+++++++++++++++\
             EEG = pop_artdiff( EEG , 'Channel',  1:32, 'Flag',  8, 'Review', 'off', 'Threshold',  artparms(s,18), 'Twindow', [ -200 900] ); %Extreme spikiness
          end    
         
+% GARV: when running artif_one, add single line artifact detection functions after default artparm lines above. 
+% Also add these 3 lines to artif for each subject
+% before: EEG.setname= [subject{s} sfx x]; to plot EEG, pause to scroll EEG, before save
+% Uncomment the 2 lines to use; comment out to not run
+%        pop_eegplot( EEG, 1, 1, 1); 
+%        fprintf('\n+++++++++++++++\n+ Hit return to save or Ctrl-c to quit and try again with new threshold ... \n+++++++++++++++\n\n', sfx);
+%        pause();    % Pause to scroll EEG plot
+% delete above 3 lines when all parms done, to run artif_all
+
         EEG.setname= [subject{s} sfx x];
         
         EEG = pop_saveset(EEG, 'filename', [EEG.setname set], 'filepath', data_path{s});   
