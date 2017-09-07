05aa4955167df830d67c90b4a64ba2be2b56da2f
David Bridges <david-bridges@hotmail.co.uk>
2017-09-07 15:33:09 +0100

    Update std_erprefch.m
    Adds conditional statements to determine variables for CAR or mastoid referencing.

--- std_erprefch.m
+++ std_erprefch.m
@@ -5,100 +5,55 @@ function std_erprefch
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 global M12 nraw pathname_all data_path subject CA ch64 all one s n
-
+ 
 disp('ERP re-reference: Working ...')
+% Set filename for text file holding re-referencing channal operations
+% Set ERP suffix
+% Set reference type for printed report
+if M12
+    erpREFtext = ['ERPref64M12.txt'];
+    erp_ext = '_armM';
+    ref_type = 'MASTOIDS';
+elseif CA
+    erpREFtext = ['ERPref64CAR.txt'];
+    erp_ext = '_armCAR';
+    ref_type = 'COMMON AVERAGE';
+end
 
 if one
+    sname = [data_path{n} subject{n} '_ar.erp'];
+    if exist(sname, 'file')>0
+        clear ERP;  % Clear is to make sure that we start fresh for each subject with the EEG and ERP structures
+        ERP = pop_loaderp('filename', [subject{n} '_ar.erp'] , 'filepath', data_path{n});
+        ERP = pop_erpchanoperator( ERP, erpREFtext, 'ErrorMsg', 'popup' );
+        % ERP = pop_erpchanoperator( ERP, [pathname_all 'ERPref64M12.txt'] , 'ErrorMsg', 'popup' );
+        erpname = [subject{n} erp_ext];  % name for erpset menu
+        fname_erp = fullfile(data_path{n}, [erpname '.erp']);
+        pop_savemyerp(ERP, 'erpname', erpname, 'filename', fname_erp, 'Warning', 'off');
+    else
+        fprintf('\n *** WARNING: %s does not exist *** \n\n', sname);
+    end
     
-    if M12
-        sname = [data_path{n} subject{n} '_ar.erp'];
-        if exist(sname, 'file')>0               
-            clear ERP;  % Clear is to make sure that we start fresh for each subject with the EEG and ERP structures    
-            ERP = pop_loaderp('filename', [subject{n} '_ar.erp'] , 'filepath', data_path{n});
-            ERP = pop_erpchanoperator( ERP, ['ERPref64M12.txt'] , 'ErrorMsg', 'popup' );
-           % ERP = pop_erpchanoperator( ERP, [pathname_all 'ERPref64M12.txt'] , 'ErrorMsg', 'popup' );
-            erpname = [subject{n} '_armM'];  % name for erpset menu
-            fname_erp = fullfile(data_path{n}, [erpname '.erp']);
+    fprintf('\n+++++++++++++++\n+ ERP Average %s Re-Reference: done! ... \n+++++++++++++++\n\n',ref_type);
+ 
+end
+
+if all     
+    for s=1:nraw
+        sname = [data_path{s} subject{s} '_ar.erp'];
+        if exist(sname, 'file')>0
+            clear ERP;  % Clear is to make sure that we start fresh for each subject with the EEG and ERP structures
+            ERP = pop_loaderp('filename', [subject{s} '_ar.erp'] , 'filepath', data_path{s});
+            ERP = pop_erpchanoperator( ERP, erpREFtext , 'ErrorMsg', 'popup' );
+            % ERP = pop_erpchanoperator( ERP, [pathname_all 'ERPref64M12.txt'] , 'ErrorMsg', 'popup' );
+            erpname = [subject{s} erp_ext];  % name for erpset menu
+            fname_erp = fullfile(data_path{s}, [erpname '.erp']);
             pop_savemyerp(ERP, 'erpname', erpname, 'filename', fname_erp, 'Warning', 'off');
         else
             fprintf('\n *** WARNING: %s does not exist *** \n\n', sname);
         end
-         
-        fprintf('\n+++++++++++++++\n+ ERP Average Mastoids Re-Reference: done! ... \n+++++++++++++++\n\n');   
-    end    
-
-    if CA
-        sname = [data_path{n} subject{n} '_ar.erp'];
-        if exist(sname, 'file')>0               
-            clear ERP;  % Clear is to make sure that we start fresh for each subject with the EEG and ERP structures    
-            ERP = pop_loaderp('filename', [subject{n} '_ar.erp'] , 'filepath', data_path{n});
-                if ch64 % 64 channel cap            
-                    ERP = pop_erpchanoperator( ERP, { 'ch70 = ch65 - ch66 label HE' 'ch71 = ch34 - ch67 label VE' 'ch72 = ch34 label Fp2c' 'ch73 = ch67 label LEc' });
-                 % ERP = pop_erpchanoperator( ERP, [pathname_all 'ERPref64CAR.txt'] , 'ErrorMsg', 'popup' );% or create a txt file 'ERPref64CAR.txt' for 64 channels
-                else    % 32 channel cap
-                    ERP = pop_erpchanoperator( ERP, { 'ch38 = ch33 - ch34 label HE' 'ch39 = ch30 - ch35 label VE' 'ch40 = ch30 label Fp2c' 'ch41 = ch35 label LEc'});
-                 %  ERP = pop_erpchanoperator( ERP, [pathname_all  'ERPref32CAR.txt'] , 'ErrorMsg', 'popup' );% or create a txt file 'ERPref32CAR.txt' for 32 channels 
-                end
-
-                erpname = [subject{n} '_arCA'];  % name for erpset menu
-                fname_erp= fullfile(data_path{n}, [erpname '.erp']);
-                pop_savemyerp(ERP, 'erpname', erpname, 'filename', fname_erp, 'Warning', 'off');
-
-            else
-            fprintf('\n *** WARNING: %s does not exist *** \n\n', sname);
-        end
-
-        fprintf('\n+++++++++++++++\n+ ERP Common Average Re-Reference: done! ... \n+++++++++++++++\n\n');   
-    end     
+    end
+    fprintf('\n+++++++++++++++\n+ ERP Average %s Re-Reference: done! ... \n+++++++++++++++\n\n',ref_type);
 end
 
-%optional
-if all     
-    if M12
-
-        for s=1:nraw
-            sname = [data_path{s} subject{s} '_ar.erp'];
-            if exist(sname, 'file')>0               
-                clear ERP;  % Clear is to make sure that we start fresh for each subject with the EEG and ERP structures    
-                ERP = pop_loaderp('filename', [subject{s} '_ar.erp'] , 'filepath', data_path{s});
-                ERP = pop_erpchanoperator( ERP, ['ERPref64M12.txt'] , 'ErrorMsg', 'popup' );
-                 % ERP = pop_erpchanoperator( ERP, [pathname_all 'ERPref64M12.txt'] , 'ErrorMsg', 'popup' );
-                erpname = [subject{s} '_armM'];  % name for erpset menu
-                fname_erp = fullfile(data_path{s}, [erpname '.erp']);
-                pop_savemyerp(ERP, 'erpname', erpname, 'filename', fname_erp, 'Warning', 'off');
-            else
-                fprintf('\n *** WARNING: %s does not exist *** \n\n', sname);
-            end
-        end 
-        fprintf('\n+++++++++++++++\n+ ERP Average Mastoids Re-Reference: done! ... \n+++++++++++++++\n\n');   
-    end    
-
-    if CA
-        for s=1:nraw
-            sname = [data_path{s} subject{s} '_ar.erp'];
-            if exist(sname, 'file')>0               
-                clear ERP;  % Clear is to make sure that we start fresh for each subject with the EEG and ERP structures    
-                ERP = pop_loaderp('filename', [subject{s} '_ar.erp'] , 'filepath', data_path{s});
-                    if ch64 % 64 channel cap            
-                        ERP = pop_erpchanoperator( ERP, { 'ch70 = ch65 - ch66 label HE' 'ch71 = ch34 - ch67 label VE' 'ch72 = ch34 label Fp2c' 'ch73 = ch67 label LEc' });
-                     % ERP = pop_erpchanoperator( ERP, [pathname_all 'ERPref64CAR.txt'] , 'ErrorMsg', 'popup' );% or create a txt file 'ERPref64CAR.txt' for 64 channels
-                    else    % 32 channel cap
-                        ERP = pop_erpchanoperator( ERP, { 'ch38 = ch33 - ch34 label HE' 'ch39 = ch30 - ch35 label VE' 'ch40 = ch30 label Fp2c' 'ch41 = ch35 label LEc'});
-                     %  ERP = pop_erpchanoperator( ERP, [pathname_all  'ERPref32CAR.txt'] , 'ErrorMsg', 'popup' );% or create a txt file 'ERPref32CAR.txt' for 32 channels 
-                    end
-
-                    erpname = [subject{s} '_arCA'];  % name for erpset menu
-                    fname_erp= fullfile(data_path{s}, [erpname '.erp']);
-                    pop_savemyerp(ERP, 'erpname', erpname, 'filename', fname_erp, 'Warning', 'off');
-
-                else
-                fprintf('\n *** WARNING: %s does not exist *** \n\n', sname);
-            end
-        end
-            fprintf('\n+++++++++++++++\n+ ERP Common Average Re-Reference: done! ... \n+++++++++++++++\n\n');   
-    end 
 end
-
-    
-    
-        
