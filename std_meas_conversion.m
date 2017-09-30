99ffb4d20031c2389a71d6ddba14edf1a1c9229f
David Bridges <david-bridges@hotmail.co.uk>
2017-09-30 03:41:27 +0100

    Update std_meas_conversion.m
    Adds column names, showing electrode by bin.

--- std_meas_conversion.m
+++ std_meas_conversion.m
@@ -6,36 +6,36 @@ function std_meas_conversion
 
 global pathname_meas ch64
 
-channels=[64,32];
 disp('Converting .txt measurements to .csv...')
+channels=[64,32];
 dirs = dir(pathname_meas);
 counter=1;
-
+bin_num=1;
 col_labs={};
-for i = 1:length(dir(pathname_meas));
-    
+for i = 1:length(dir(pathname_meas));    
     if strfind(dirs(i).name,'All.txt');
         datas = readtable(dirs(i).name,'delimiter','\t');
         ncols = (height(datas)/std_erp_count)  ;      
         for j = 1:(ncols)
             if counter < channels(2-ch64)
-                col_labs{j}={strcat(char(datas(j,'chlabel').chlabel),'_',int2str((counter)))};
+                col_labs{j}={strcat(char(datas(j,'chlabel').chlabel),'_',int2str((bin_num)))};
                 counter=counter+1;
             else
-                col_labs{j}={strcat(char(datas(j,'chlabel').chlabel),'_',int2str((counter)))};
+                col_labs{j}={strcat(char(datas(j,'chlabel').chlabel),'_',int2str((bin_num)))};
                 counter = 1;
+                bin_num=bin_num+1;
             end
         end
-    
     % create new matrix - participant x electrode
     transposed_column = transpose(table2cell(datas(:,'value')));
     newTable = reshape(transposed_column,ncols, std_erp_count);
-    newTable = transpose(newTable);
-    newTable = array2table(newTable);
-%     newTable.Properties.VariableNames={transpose(col_labs)};
+    newTable = array2table(transpose(newTable));
+    % Change column names
+    for new_column_names = 1:length(col_labs)
+        newTable.Properties.VariableNames(new_column_names)=col_labs{new_column_names};
+    end 
     disp('Writing measurements to .csv file...')
     writetable(newTable,[pathname_meas strcat(dirs(i).name(1:length(dirs(i).name)-3),'csv')],'Delimiter',',');
-
     end
 end
 
