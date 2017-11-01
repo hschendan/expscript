dcf660334ea5d21fa33d2d08b10e2c4d6c1d533c
hschendan <haline_schendan@fastmail.us>
2017-11-01 12:58:15 +0000

    Update std_erpfilt.m

--- std_erpfilt.m
+++ std_erpfilt.m
@@ -1,16 +1,14 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Basic Filtering for ERP with Low Pass Pg. 36
+%Basic Filtering for ERP with Low Pass 40 Hz (default; change to yours) Pg. 36
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function std_erpfilt
 
-%global nraw data_path subject ch64 sfx set
 global M12 nraw pathname_all data_path subject CA ch64 all one s n
-
    
 disp('ERP Filter: Working ...')
 
-% currently, default load is armM: 
-% M12 vs. CA not fully implemented; need to change below to load armM or arCA; 
+
+% flags must be set to M12 = 1 or CA = 1 or both M12 and CA = 0 (no reref). Add 'f' to end of filename to name output
 if M12
     erp_ext = '_armMf';
     ref_type = 'MASTOIDS';
@@ -26,13 +24,16 @@ if one
     % NOT IMPLEMENTED: to script copy one section from erprefch.m
 end
 
+% filter erp
+
 if all   
-    %for s=1:nraw %   
-     for s=[18 28 35]
-%    for s=20 % one subject quick solution
+    for s=1:nraw   
+   %  for s=[18 28 35]
+   %  for s=20 % one subject quick solution
         sname = [data_path{s} subject{s} '_armM.erp'];
         if exist(sname, 'file')>0
             clear ERP;  % Clear is to make sure that we start fresh for each subject with the EEG and ERP structures
+            % Load input file M12 (mM), CA, or recording reference
             if M12
                 ERP = pop_loaderp('filename', [subject{s} '_armM.erp'] , 'filepath', data_path{s});
             elseif CA
@@ -41,15 +42,14 @@ if all
                 ERP = pop_loaderp('filename', [subject{s} '_ar.erp'] , 'filepath', data_path{s});
             end
             % SAI
-            ERP = pop_filterp( ERP,  1:65 , 'Cutoff',  40, 'Design', 'butter', 'Filter', 'lowpass', 'Order',  2 );
+            % ERP = pop_filterp( ERP,  1:65 , 'Cutoff',  40, 'Design', 'butter', 'Filter', 'lowpass', 'Order',  2 );
             % BIOSEMI
-%           ERP = pop_filterp( ERP,  1:71 , 'Cutoff',  30, 'Design', 'butter', 'Filter', 'lowpass', 'Order',  2 );
-%            else    % 32 channel
-%                ERP = pop_filterp( ERP,  1:39 , 'Cutoff',  30, 'Design', 'butter', 'Filter', 'lowpass', 'Order',  2 );
-%            end
-     
-            %ERP = pop_erpchanoperator( ERP, erpREFtext , 'ErrorMsg', 'popup' );
-            % ERP = pop_erpchanoperator( ERP, [pathname_all 'ERPref64M12.txt'] , 'ErrorMsg', 'popup' );
+               ERP = pop_filterp( ERP,  1:71 , 'Cutoff',  40, 'Design', 'butter', 'Filter', 'lowpass', 'Order',  2 );
+            else    % 32 channel
+               ERP = pop_filterp( ERP,  1:39 , 'Cutoff',  40, 'Design', 'butter', 'Filter', 'lowpass', 'Order',  2 );
+            end
+
+% name and save     
             erpname = [subject{s} erp_ext];  % name for erpset menu
             fname_erp = fullfile(data_path{s}, [erpname '.erp']);
             pop_savemyerp(ERP, 'erpname', erpname, 'filename', fname_erp, 'Warning', 'off');
